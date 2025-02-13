b08e8292f4afa3d0dc22514a1e23b56fac6a271e
Felim Sullivan <felim.sullivan@gmail.com>
2025-02-13 12:45:35 +0000

    sorted-boolean-to-fix-reset

--- src/Panel.java
+++ src/Panel.java
@@ -3,7 +3,6 @@ import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.Comparator;
-import java.util.Random;
 
 public class Panel extends JPanel {
     /** Singleton class **/
